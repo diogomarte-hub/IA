ebeaf7d17ef5832c5ccdc54b4c9eaa1a49a56db6
diogomarte-hub <diogomarte@gmail.com>
2025-10-19 22:01:28 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,12 +33,6 @@ function App() {
     }
   }, [userId]);
 
-  useEffect(() => {
-    if (userId) {
-      loadUserProgress();
-    }
-  }, [userId]);
-
   async function checkUser() {
     const { data: { session } } = await supabase.auth.getSession();
     if (session?.user) {
